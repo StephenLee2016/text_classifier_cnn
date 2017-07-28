0befe734c447c24c90da9ac836e1d783ff5c3464
jrlimingyang <jrlimingyang@jd.com>
2017-07-28 17:33:16 +0800

    add train_model.py

--- corpus_handle.py
+++ corpus_handle.py
@@ -42,81 +42,88 @@ def load_data_and_labels():
     what_examples = list(open('C:\\Users\\jrlimingyang\\PycharmProjects\\question_classifier_cnn\\corpus\\qa_data.what', 'r').readlines())
     what_examples = [s.strip() for s in what_examples]
 
-    # split by words
-    x_text = who_examples + when_examples + what_examples # list 格式
+    # Split by words
+    x_text = who_examples + when_examples + what_examples
     x_text = [clean_str(sent) for sent in x_text]
     x_text = [s.split(" ") for s in x_text]
     # Generate labels
-    who_labels = [[1,0,0] for _ in who_examples]
-    when_labels = [[0,1,0] for _ in when_examples]
-    what_labels = [[0,0,1] for _ in what_examples]
+    who_labels = [[1, 0, 0] for _ in who_examples]
+    when_labels = [[0, 1, 0] for _ in when_examples]
+    what_labels = [[0, 0, 1] for _ in what_examples]
 
-    y = np.concatenate([who_examples, when_examples, what_examples], 0)
+    y = np.concatenate([who_labels, when_labels, what_labels], 0)
     return [x_text, y]
 
-def pad_sentences(sentences, padding_word='<PAD/>'):
-    '''
-    将所有句子都pad到相同长度，长度取决于最长的句子
-    :return: pad后的sentences
-    '''
+
+def pad_sentences(sentences, padding_word="<PAD/>"):
+    """
+    Pads all sentences to the same length. The length is defined by the longest sentence.
+    Returns padded sentences.
+    """
     sequence_length = max(len(x) for x in sentences)
     padded_sentences = []
     for i in range(len(sentences)):
         sentence = sentences[i]
         num_padding = sequence_length - len(sentence)
-        new_sentence = sentence + num_padding * [padding_word]
+        new_sentence = sentence + [padding_word] * num_padding
         padded_sentences.append(new_sentence)
     return padded_sentences
 
 
 def build_vocab(sentences):
-    '''
-    建立词到索引和索引到词的映射关系
-    :return: word2index 和 index2word 字典
-    '''
+    """
+    Builds a vocabulary mapping from word to index based on the sentences.
+    Returns vocabulary mapping and inverse vocabulary mapping.
+    """
+    # Build vocabulary
     word_counts = Counter(itertools.chain(*sentences))
-    # Mapping index to word
+    # Mapping from index to word
     vocabulary_inv = [x[0] for x in word_counts.most_common()]
     vocabulary_inv = list(sorted(vocabulary_inv))
-    # Mapping word to index
-    vocabulary = {x:i for i, x in enumerate(vocabulary_inv)}
+    # Mapping from word to index
+    vocabulary = {x: i for i, x in enumerate(vocabulary_inv)}
     return [vocabulary, vocabulary_inv]
 
+
 def build_input_data(sentences, labels, vocabulary):
-    '''
-    将sentence用词的索引表示（表示成数字）
-    '''
-    x = np.array([[vocabulary[word] for word in sentence] for sentence in sentences ])
+    """
+    Maps sentencs and labels to vectors based on a vocabulary.
+    """
+    x = np.array([[vocabulary[word] for word in sentence] for sentence in sentences])
     y = np.array(labels)
     return [x, y]
 
+
 def load_data():
-    '''
-    加载并处理数据集
-    :return: 句子的vector, labels, 词和索引的映射和索引到词的映射关系
-    '''
+    """
+    Loads and preprocessed data for dataset.
+    Returns input vectors, labels, vocabulary, and inverse vocabulary.
+    """
+    # Load and preprocess data
     sentences, labels = load_data_and_labels()
     sentences_padded = pad_sentences(sentences)
     vocabulary, vocabulary_inv = build_vocab(sentences_padded)
     x, y = build_input_data(sentences_padded, labels, vocabulary)
     return [x, y, vocabulary, vocabulary_inv]
 
+
 def batch_iter(data, batch_size, num_epochs, shuffle=True):
-    '''
-    产生训练的 batch 数据
-    '''
+    """
+    Generates a batch iterator for a dataset.
+    """
     data = np.array(data)
     data_size = len(data)
-    num_batches_per_epoch = int(len(data)/batch_size) + 1
+    num_batches_per_epoch = int(len(data) / batch_size) + 1
     for epoch in range(num_epochs):
-        # 将每个batch内的数打乱
+        # Shuffle the data at each epoch
         if shuffle:
             shuffle_indices = np.random.permutation(np.arange(data_size))
             shuffled_data = data[shuffle_indices]
         else:
             shuffled_data = data
-
         for batch_num in range(num_batches_per_epoch):
             start_index = batch_num * batch_size
             end_index = min((batch_num + 1) * batch_size, data_size)
-            yield shuffled_data[start_index : end_index]
+            yield shuffled_data[start_index:end_index]
+
+            # x_test, y_test, vocabulary, vocabulary_inv = load_data()
